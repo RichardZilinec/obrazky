9c8ed1e5ed18231e4965dd8f334d7f845f34c30d
RichardZilinec <zilinecrichard@gmail.com>
2026-04-01 09:19:19 +0200

    Refactor picture unshreder and add image creation
    Refactored the picture unshreder function and added functionality to create an image from a text file.

--- unshreder.py
+++ unshreder.py
@@ -1,36 +1,15 @@
 from PIL import Image
-def picture_unshreder(pic):
-    obr = Image.open(pic)
-    pixels = obr.load()
-    bin_message = ""
-    vysledny_text = ""
-    index = 0
-    
-    while True:
-        x = index % obr.size[0]
-        y = index // obr.size[0]
 
-        blue_val = pixels[x, y][2]
-        posledny_bit = bin(blue_val)[-1]
-        bin_message += posledny_bit
+text_seda = open("ciernobiely_obrazok_1.txt", "r")
+first_line = text_seda.readline()
+size = first_line.split(" ")
+pic = Image.new("RGB", (int(size[0]), int(size[1])),"white")
+pixel = pic.load()
 
-        if len(bin_message) == 7:
-            znak = chr(int(bin_message, 2))
-
-            if znak == '#':
-                break
-                
-            vysledny_text += znak
-            bin_message = ""
-            
-        index += 1
-
-        if index >= obr.size[0] * obr.size[1]:
-            break
-            
-    return vysledny_text
-
-
-obr_out = 'final_pic17.png'
-
-print(picture_unshreder(obr_out))
+for y in range(pic.size[1]):
+    lines = text_seda.readline()
+    for x in range(pic.size[0]):
+        color = lines[x*2:x*2+1]
+        if color:
+            pixel[x,y] = (int(color,16), int(color,16), int(color,16))
+pic.show()
